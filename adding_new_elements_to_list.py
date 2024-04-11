7cb3c60d1bc2a8d83aff2f09bf7e8fd25c3c6c2c
Dewayne Parker <mr.leonard.parker@gmail.com>
2024-04-11 01:00:32 +0000

    adding_new_ElementsToList

---
+++ adding_new_elements_to_list.py
@@ -0,0 +1,14 @@
+#print('hello')
+
+#top_cities = ['New York City', 'Los Angeles', 'Chicago', 'Houston', 'Phoenix']
+#print(top_cities)
+
+#name = inout('name?')
+#book_ratings = [7, 9, 5, 6, 8]
+#book_ratings.append(4)
+#print(book_ratings)
+
+# method invocation adds the value of ten.
+book_ratings = [7, 9, 5, 6, 8]
+book_ratings.insert(1, 10)
+print(book_ratings)
