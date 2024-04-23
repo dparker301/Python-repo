782ac010c3e029fba737c29d3322450d9d692f50
Dewayne Parker <mr.leonard.parker@gmail.com>
2024-04-23 00:24:11 +0000

    names_scope

---
+++ default_parameter_values.py
@@ -0,0 +1,16 @@
+# Positional arguments must appear first and only then can named arguments appear.
+
+print('Hello', 'How are you?', end='.', sep='-')
+
+
+
+def print_letter_count(text='This is the default string to search', letter='a'):
+    counter = 0
+    for char in text:
+        if char == letter:
+            counter += 1
+    print('Number of', letter, 'is', counter)
+
+print_letter_count('How many letters a are here?')
+
+print_letter_count('Search here', letter='a')
